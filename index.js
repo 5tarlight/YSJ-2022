ba9c450c6ff4a3f8223124afdb4b4079fd426a03
YEAHx4 <developerstarlight@gmail.com>
2022-09-24 17:00:30 +0900

    copy to clipboard

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand("copy");
+    var msg = successful ? "successful" : "unsuccessful";
+    console.log("Fallback: Copying text command was " + msg);
+  } catch (err) {
+    console.error("Fallback: Oops, unable to copy", err);
+  }
+
+  document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(
+    function () {
+      console.log("Async: Copying to clipboard was successful!");
+    },
+    function (err) {
+      console.error("Async: Could not copy text: ", err);
+    }
+  );
+}
+
 function start() {
   const birth = moment("2005-09-27");
   const birthday = moment("2022-09-27");
@@ -24,8 +63,11 @@ function start() {
         "2시간동안 만든거다 보니까 퀄리티도 많이 낮고 디테일도 없는데 그 부분에 대해서 사죄드립니다. " +
         "그래도 1년만의 생일에 일본어 100번 쓰느라 팔에 근육생겼을텐데 윤소정님의 앞날에 꽃길만 있기를 기원합니다. " +
         "사실 아까도 말했듯 뭐 축하하고 이런거 하나도 할줄 모르는 저이기에 아래의 마지막 선물을 드리려고 합니다. " +
-        "힌트는 Base64 복호화(decrypt)입니다.\n\n" +
-        "7Jyk7IaM7KCV7J2YIOunjCAxN+yCtOuQqOydhCDstpXtlZjtlanri4jri6QhIOywqOuniCDsnbTrn7DqsbAg6re464Ol7J2AIOuqu+yTsOqyoOyWtOyEnCDsnbTroIfqsowg7JSB64uI64ukLiDsu7Tqs7XsnbTsl4jsnLzrqbQg642UIOuzteyeoe2VmOqyjCDslZTtmLjtmZTtlojsnYTthZDrjbAg6re4656Y64+EIOydtOygleuPhOuKlCDssL7snYQg7IiYIOyeiOydhOqxsOudvCDrr7/slrTsmpR+44WOLiDslrTsqYPqsbDrgpgg7J207KCcIOuvvOymneuPhCDrgpjsmKTripQg64KY7J206rCAIOuQmOyXiOqzoCDsi5ztl5jrj4Qg64KY7J2067O064ukIOyggeqyjCDrgqjslZjripTrjbAg7IOd7J287J2064uI6rmMIOuEiOustCDqs7XrtoDrp4wg7ZWc64uk6rOgIOyKpO2KuOugiOyKpOuwm+yngCDrp5Dqs6Ag7ZaJ67O17ZWcIO2VmOujqCDrs7TrgrTshajsnLzrqbQg7KKL6rKg7Iq164uI64ukLiAy7ZWZ6riwIOyEseyggeuPhCDsnpgg64KY7Jik6ri4KOuCtCDrsJTroZwg65KkIOOFjikg67CU65286rKg7Iq164uI64ukLg=="
+        "힌트는 Base64 복호화(decrypt)입니다. 클립보드를 확인해보세요."
+    );
+
+    copyTextToClipboard(
+      '"7Jyk7IaM7KCV7J2YIOunjCAxN+yCtOuQqOydhCDstpXtlZjtlanri4jri6QhIOywqOuniCDsnbTrn7DqsbAg6re464Ol7J2AIOuqu+yTsOqyoOyWtOyEnCDsnbTroIfqsowg7JSB64uI64ukLiDsu7Tqs7XsnbTsl4jsnLzrqbQg642UIOuzteyeoe2VmOqyjCDslZTtmLjtmZTtlojsnYTthZDrjbAg6re4656Y64+EIOydtOygleuPhOuKlCDssL7snYQg7IiYIOyeiOydhOqxsOudvCDrr7/slrTsmpR+44WOLiDslrTsqYPqsbDrgpgg7J207KCcIOuvvOymneuPhCDrgpjsmKTripQg64KY7J206rCAIOuQmOyXiOqzoCDsi5ztl5jrj4Qg64KY7J2067O064ukIOyggeqyjCDrgqjslZjripTrjbAg7IOd7J287J2064uI6rmMIOuEiOustCDqs7XrtoDrp4wg7ZWc64uk6rOgIOyKpO2KuOugiOyKpOuwm+yngCDrp5Dqs6Ag7ZaJ67O17ZWcIO2VmOujqCDrs7TrgrTshajsnLzrqbQg7KKL6rKg7Iq164uI64ukLiAy7ZWZ6riwIOyEseyggeuPhCDsnpgg64KY7Jik6ri4KOuCtCDrsJTroZwg65KkIOOFjikg67CU65286rKg7Iq164uI64ukLg=="'
     );
   };
 }
